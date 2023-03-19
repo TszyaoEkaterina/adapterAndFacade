cc406731e1140eddf18bc7b7ce6db0dded17efed
TszyaoEkaterina <3022805283@qq.com>
2023-03-18 21:14:15 -0400

    HW2-facade finished

---
+++ src/main/java/facade/Main.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println("10 + 10101 in binary= "+bins.sum("10","10101"));
+        System.out.println("11101 + 10101 in binary= "+bins.sum("11101","10101"));
+        System.out.println("1001 * 1100 in binary= "+bins.mult("1001","1100"));
+        System.out.println("10 * 10101 in binary= "+bins.mult("10","10101"));
+    }
+}
